d2679f7a4edc68d2194373977a01d09adc4e16ee
Alfread Rorw <alfreadrorw@gmail.com>
2026-01-17 01:56:39 +0800

    updateror12

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,16 @@
 // firebase-config.js
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDBNHEbhrQBfNlcs2IJt3Fui--1-jkTv54",
-  authDomain: "family100-2f913.firebaseapp.com",
-  databaseURL: "https://family100-2f913-default-rtdb.firebaseio.com",
-  projectId: "family100-2f913",
-  storageBucket: "family100-2f913.firebasestorage.app",
-  messagingSenderId: "536609229146",
-  appId: "1:536609229146:web:62b2e4194de0db93f30b63",
-  measurementId: "G-NCCEEZRFV1"
+  apiKey: "AIzaSyCTbLlKjUKI9SMVgnNgaqj9ivScgxoxynI",
+  authDomain: "games-592c5.firebaseapp.com",
+  databaseURL: "https://games-592c5-default-rtdb.firebaseio.com",
+  projectId: "games-592c5",
+  storageBucket: "games-592c5.firebasestorage.app",
+  messagingSenderId: "103017684838",
+  appId: "1:103017684838:web:86d5651cd44d715bc132a7",
+  measurementId: "G-LQE1MMV6TY"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
