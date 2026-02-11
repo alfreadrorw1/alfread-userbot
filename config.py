b35f8b3c37c258a1da86430a1eb5ebe3bba1c76c
alfreadrorw1 <alfreadrorw@gmail.com>
2026-02-11 16:32:05 +0800

    update

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ from telethon.errors import SessionPasswordNeededError
 # API credentials
 API_ID = 24616632  # Ganti dengan API ID Anda
 API_HASH = "0af20782aecff3ff206cd2b66c04e149"  # Ganti dengan API Hash Anda
-BOT_TOKEN = "8554556698:AAE-W8djmFG8gn9-Q2sJYSjwQTrjNwBXUHQ"  # Ganti dengan bot token utama
+BOT_TOKEN = "8118603438:AAE4CCUE_VMVSSIWxWu5heAW5JwkS72wSRM"  # Ganti dengan bot token utama
 
 
 # Admin/user IDs
-OWNER_ID = 8337278265  # Ganti dengan ID Anda
+OWNER_ID = 5789407694  # Ganti dengan ID Anda
